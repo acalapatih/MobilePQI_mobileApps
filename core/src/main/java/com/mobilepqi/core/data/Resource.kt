ed96936693c9be63a3ecfaa9c8f01093e9ae1e61
muhammadaqil18 <aqil.pratama@koinworks.com>
2023-01-22 17:23:34 +0700

    cleanup code

--- core/src/main/java/com/mobilepqi/core/data/Resource.kt
+++ core/src/main/java/com/mobilepqi/core/data/Resource.kt
@@ -2,6 +2,6 @@ package com.mobilepqi.core.data
 
 sealed class Resource<T>(var data: T? = null, val message: String? = null) {
     class Success<T>(data: T) : Resource<T>(data)
-    class Loading<T>(data: T? = null) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+    class Loading<T> : Resource<T>()
+    class Error<T>(message: String) : Resource<T>(message = message)
 }
