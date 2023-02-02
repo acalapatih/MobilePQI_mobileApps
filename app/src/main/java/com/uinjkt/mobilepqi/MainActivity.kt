8e9c3c7e7d30456e0778258de944a8954de86fb2
Acalapatih <76055633+acalapatih@users.noreply.github.com>
2023-02-02 22:29:57 +0700

    updt

--- app/src/main/java/com/uinjkt/mobilepqi/MainActivity.kt
+++ app/src/main/java/com/uinjkt/mobilepqi/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.profil)
+        setContentView(R.layout.profil_pengaturan)
         supportActionBar?.hide()
     }
 }
