53912f6fb836c3995b906aa71aa395fae5c95047
Acalapatih <76055633+acalapatih@users.noreply.github.com>
2023-02-05 11:32:40 +0700

    updt

--- app/src/main/java/com/uinjkt/mobilepqi/Activity2.kt
+++ app/src/main/java/com/uinjkt/mobilepqi/Activity2.kt
@@ -4,6 +4,9 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ImageView
 
 class Activity2 : Activity() {
@@ -19,5 +22,10 @@ class Activity2 : Activity() {
             setResult(RESULT_OK, intent)
             finish()
         }
+
+        val imgFakultas = findViewById<ImageButton>(R.id.img_edit_fakultas)
+        val editFakultas = findViewById<EditText>(R.id.input_fakultas)
+
+        imgFakultas.setOnClickListener()
     }
 }
