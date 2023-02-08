b57387265614b464c0984cdd12c42664805db9d7
Acalapatih <76055633+acalapatih@users.noreply.github.com>
2023-02-08 18:18:16 +0700

    update

---
+++ app/src/main/java/com/uinjkt/mobilepqi/ui/BuatKelasActivity.kt
@@ -0,0 +1,29 @@
+package com.uinjkt.mobilepqi.ui
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.uinjkt.mobilepqi.common.BaseActivity
+import com.uinjkt.mobilepqi.databinding.ActivityBuatKelasBinding
+
+class BuatKelasActivity: BaseActivity<ActivityBuatKelasBinding>() {
+    companion object{
+        @JvmStatic
+        fun start(context: Context) {
+            val starter = Intent(context, DaftarKelasActivity::class.java)
+            context.startActivity(starter)
+        }
+    }
+
+    override fun getViewBinding(): ActivityBuatKelasBinding = ActivityBuatKelasBinding.inflate(layoutInflater)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        val backIntent = binding.icClose
+        backIntent.setOnClickListener {view ->
+            val backIntent = Intent(view.context, DaftarKelasActivity::class.java)
+            startActivityForResult(backIntent, 0)
+        }
+    }
+}
---
+++ app/src/main/java/com/uinjkt/mobilepqi/ui/DaftarKelasActivity.kt
@@ -0,0 +1,34 @@
+package com.uinjkt.mobilepqi.ui
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.uinjkt.mobilepqi.common.BaseActivity
+import com.uinjkt.mobilepqi.databinding.ActivityDaftarKelasBinding
+
+class DaftarKelasActivity: BaseActivity<ActivityDaftarKelasBinding>() {
+    companion object{
+        @JvmStatic
+        fun start(context: Context) {
+            val starter = Intent(context, DaftarKelasActivity::class.java)
+            context.startActivity(starter)
+        }
+    }
+
+    override fun getViewBinding(): ActivityDaftarKelasBinding = ActivityDaftarKelasBinding.inflate(layoutInflater)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        val buatkelas = binding.icBuatkelas
+        buatkelas.setOnClickListener {view ->
+            val buatkelasIntent = Intent(view.context, BuatKelasActivity::class.java)
+            startActivityForResult(buatkelasIntent, 0)
+        }
+        val buatkelas2 = binding.tvBuatkelas
+        buatkelas2.setOnClickListener {view ->
+            val buatkelasIntent = Intent(view.context, BuatKelasActivity::class.java)
+            startActivityForResult(buatkelasIntent, 0)
+        }
+    }
+}
