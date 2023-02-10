f61d0cf36539426f4f18ec730314b11f9e78e729
muhammadaqil18 <aqil.pratama@koinworks.com>
2023-02-10 22:17:32 +0700

    add injection

--- app/src/main/java/com/uinjkt/mobilepqi/di/AppModule.kt
+++ app/src/main/java/com/uinjkt/mobilepqi/di/AppModule.kt
@@ -1,11 +1,18 @@
 package com.uinjkt.mobilepqi.di
 
+import com.mobilepqi.core.domain.usecase.JadwalSholatInteractor
+import com.mobilepqi.core.domain.usecase.JadwalSholatUsecase
+import com.uinjkt.mobilepqi.ui.dashboard.DashboardViewModel
+import com.uinjkt.mobilepqi.ui.signin.SignInViewModel
+import com.uinjkt.mobilepqi.ui.signin.SigninActivity
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val useCaseModule = module {
-
+    factory<JadwalSholatUsecase> { JadwalSholatInteractor(get()) }
 }
 
 val viewModelModule = module {
-
+    viewModel { DashboardViewModel(get()) }
+    viewModel { SignInViewModel() }
 }
--- core/src/main/java/com/mobilepqi/core/di/CoreModule.kt
+++ core/src/main/java/com/mobilepqi/core/di/CoreModule.kt
@@ -1,11 +1,41 @@
 package com.mobilepqi.core.di
 
+import com.mobilepqi.core.BuildConfig
+import com.mobilepqi.core.data.repository.JadwalSholatRepositoryImpl
+import com.mobilepqi.core.data.source.remote.RemoteDataSource
+import com.mobilepqi.core.data.source.remote.network.ApiSholatService
+import com.mobilepqi.core.domain.repository.JadwalSholatRepository
+import java.util.concurrent.TimeUnit
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
-
+    single {
+        val loggingInterceptor = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        } else {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+        }
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+    single {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.ENDPOINT_API_JADWAL_SHOLAT)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+        retrofit.create(ApiSholatService::class.java)
+    }
 }
 
 val repositoryModule = module {
-
+    single { RemoteDataSource(get()) }
+    single<JadwalSholatRepository> { JadwalSholatRepositoryImpl(get()) }
 }
