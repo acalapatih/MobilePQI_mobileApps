2d4a429ae324a325f95d6007b4950596ef6b3348
Ammar Sufyan <63483228+neouranos@users.noreply.github.com>
2023-02-10 19:47:00 +0700

    create ui -> dosen -> menusilabus package

---
+++ app/src/main/java/com/uinjkt/mobilepqi/ui/dosen/menusilabus/DosenSilabusActivity.kt
@@ -0,0 +1,26 @@
+package com.uinjkt.mobilepqi.ui.dosen.menusilabus
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.uinjkt.mobilepqi.R
+import com.uinjkt.mobilepqi.common.BaseActivity
+import com.uinjkt.mobilepqi.databinding.ActivityDosenSilabusBinding
+
+class DosenSilabusActivity : BaseActivity<ActivityDosenSilabusBinding>() {
+    companion object {
+        @JvmStatic
+        fun start(context: Context) {
+            val starter = Intent(context,ActivityDosenSilabusBinding::class.java)
+            context.startActivity(starter)
+        }
+    }
+
+    override fun getViewBinding(): ActivityDosenSilabusBinding =
+        ActivityDosenSilabusBinding.inflate(layoutInflater)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dosen_silabus)
+    }
+}
--- app/src/main/java/com/uinjkt/mobilepqi/ui/mahasiswa/menusilabus/MahasiswaSilabusActivity.kt
+++ app/src/main/java/com/uinjkt/mobilepqi/ui/mahasiswa/menusilabus/MahasiswaSilabusActivity.kt
@@ -1,10 +1,24 @@
 package com.uinjkt.mobilepqi.ui.mahasiswa.menusilabus
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.uinjkt.mobilepqi.R
+import com.uinjkt.mobilepqi.common.BaseActivity
+import com.uinjkt.mobilepqi.databinding.ActivityMahasiswaSilabusBinding
 
-class MahasiswaSilabusActivity : AppCompatActivity() {
+class MahasiswaSilabusActivity : BaseActivity<ActivityMahasiswaSilabusBinding>() {
+    companion object {
+        @JvmStatic
+        fun start(context: Context) {
+            val starter = Intent(context, ActivityMahasiswaSilabusBinding::class.java)
+            context.startActivity(starter)
+        }
+    }
+
+    override fun getViewBinding(): ActivityMahasiswaSilabusBinding =
+        ActivityMahasiswaSilabusBinding.inflate(layoutInflater)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mahasiswa_silabus)
