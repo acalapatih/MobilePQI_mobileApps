3ed0b46ed213c5791161d0f2973ab87202912494
Ammar Sufyan <63483228+neouranos@users.noreply.github.com>
2023-02-10 19:41:34 +0700

    create ui -> mahasiswa -> menusilabus package

---
+++ app/src/main/java/com/uinjkt/mobilepqi/ui/mahasiswa/menusilabus/MahasiswaSilabusActivity.kt
@@ -0,0 +1,12 @@
+package com.uinjkt.mobilepqi.ui.mahasiswa.menusilabus
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.uinjkt.mobilepqi.R
+
+class MahasiswaSilabusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mahasiswa_silabus)
+    }
+}
