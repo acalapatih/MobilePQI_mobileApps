06e55b54bcc1be101a5cbe25552e7fa8e902291e
Ammar Sufyan <63483228+neouranos@users.noreply.github.com>
2023-02-22 23:44:06 +0700

    add permission to show notifications in onboarding if android >= 13

--- app/src/main/java/com/uinjkt/mobilepqi/ui/splashscreen/OnboardingActivity.kt
+++ app/src/main/java/com/uinjkt/mobilepqi/ui/splashscreen/OnboardingActivity.kt
@@ -1,8 +1,14 @@
 package com.uinjkt.mobilepqi.ui.splashscreen
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
 import com.uinjkt.mobilepqi.common.BaseActivity
 import com.uinjkt.mobilepqi.databinding.ActivityOnboardingBinding
 import com.uinjkt.mobilepqi.ui.signin.SigninActivity
@@ -16,11 +22,28 @@ class OnboardingActivity : BaseActivity<ActivityOnboardingBinding>() {
         }
     }
 
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
     override fun getViewBinding(): ActivityOnboardingBinding =
         ActivityOnboardingBinding.inflate(layoutInflater)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                if (!it) {
+                    Snackbar.make(
+                        findViewById<View>(android.R.id.content).rootView,
+                        "Please grant Notification permission from App Settings",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
 
         binding.btnMulai.setOnClickListener {
             SigninActivity.start(this)
