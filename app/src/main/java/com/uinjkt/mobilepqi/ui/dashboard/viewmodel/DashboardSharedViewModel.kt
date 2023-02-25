3ae6ae9c5fd614e0b2dd41ae9da746d3dad1c0b9
muhammadaqil18 <aqil.pratama@koinworks.com>
2023-02-25 18:52:45 +0700

    remove unnecessary code

--- app/src/main/java/com/uinjkt/mobilepqi/ui/dashboard/viewmodel/DashboardSharedViewModel.kt
+++ app/src/main/java/com/uinjkt/mobilepqi/ui/dashboard/viewmodel/DashboardSharedViewModel.kt
@@ -1,6 +1,5 @@
 package com.uinjkt.mobilepqi.ui.dashboard.viewmodel
 
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -10,11 +9,4 @@ class DashboardSharedViewModel : ViewModel() {
     var longitude: MutableLiveData<Double> = MutableLiveData()
     var location: MutableLiveData<String> = MutableLiveData()
 
-    val a = latitude.combineWith(longitude)
-
-    fun <T, S> MutableLiveData<T>.combineWith(other: MutableLiveData<S>): MutableLiveData<Pair<T?, S?>> =
-        MediatorLiveData<Pair<T?, S?>>().apply {
-            addSource(this@combineWith) { value = Pair(it, other.value) }
-            addSource(other) { value = Pair(this@combineWith.value, it) }
-        }
 }
