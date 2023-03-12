2d3e65d6398f3f4fcd8c8859cb1c07a215a901e1
Acalapatih <76055633+acalapatih@users.noreply.github.com>
2023-03-12 23:32:20 +0700

    delete data

--- app/src/main/java/com/uinjkt/mobilepqi/data/DataMahasiswa.kt
+++
@@ -1,8 +0,0 @@
-package com.uinjkt.mobilepqi.data
-
-data class DataMahasiswa(
-    val idMahasiswa: Int,
-    val nama: String,
-    val nim: String,
-    val nimmahasiswa: Long,
-)
--- app/src/main/java/com/uinjkt/mobilepqi/data/DataTugasDashboard.kt
+++
@@ -1,9 +0,0 @@
-package com.uinjkt.mobilepqi.data
-
-data class DataTugasDashboard(
-    val idTugas: Int,
-    val tanggalTugas: String,
-    val namaTugas: String,
-    val deadlineTugas: String
-
-)
